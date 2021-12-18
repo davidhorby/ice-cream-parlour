bd04c2d1eb854dac8e6b8edcf14fa1f865c67978
David Horby <david.horby@springernature.com>
2021-12-18 13:27:23 +0000

    ASdd flavour to model

---
+++ src/main/java/com/dhorby/icecream/model/Flavour.kt
@@ -0,0 +1,14 @@
+package com.dhorby.icecream.model
+
+import java.math.BigDecimal
+
+typealias ZDR = Int
+
+enum class Flavour(val price:ZDR) {
+    ROCKY_ROAD(8),
+    COOKIE_CREAM(10) ,
+    NETFLIX_CHILL(12)
+}
+
+
+
