68834bb7427f4a069442fdd68693435392ec9305
David Horby <david.horby@springernature.com>
2021-12-18 11:36:06 +0000

    Use BigDecimals for payable item counts

--- src/test/kotlin/com/dhorby/icecream/business/BusinessRulesKtTest.kt
+++ src/test/kotlin/com/dhorby/icecream/business/BusinessRulesKtTest.kt
@@ -3,6 +3,7 @@ package com.dhorby.icecream.business
 import com.natpryce.hamkrest.equalTo
 import com.natpryce.hamkrest.assertion.assertThat
 import org.junit.jupiter.api.Test
+import java.math.BigDecimal
 
 
 class BusinessRulesKtTest {
@@ -10,7 +11,7 @@ class BusinessRulesKtTest {
     @Test
     fun buy2Get1Free() {
         val itemCount:ItemCount = 3
-        val expectedPrice = 2
-        assertThat(itemCount.buy2Get1Free(), equalTo(expectedPrice))
+        val expectedPayableItemCount = BigDecimal(2)
+        assertThat(itemCount.buy2Get1Free(), equalTo(expectedPayableItemCount))
     }
 }
